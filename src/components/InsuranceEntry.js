9037809fa6d2ce486525fd6a850ff63d6e714d93
Pieter Van Der Helst <pieter.vanderhelst@hogent.be>
2021-01-26 10:29:43 +0100

    cursor pointer on click, animate color transition

--- src/components/InsuranceEntry.js
+++ src/components/InsuranceEntry.js
@@ -2,7 +2,12 @@ export default function InsuranceEntry({ price, active, toggle }) {
 	let tdCssClass = active && price !== 0 ? ' bg-red-200' : ' bg-gray-50 ';
 	let spanCssClass = price === 0 ? 'opacity-40' : '';
 	return (
-		<td className={tdCssClass + ' px-6 py-4 whitespace-nowrap'}>
+		<td
+			className={
+				tdCssClass +
+				' px-6 py-4 whitespace-nowrap cursor-pointer transition-colors'
+			}
+		>
 			<span
 				className={spanCssClass + ' inline-grid grid-rows-3 gap-y-2 p-2 m-2'}
 				onClick={toggle}
